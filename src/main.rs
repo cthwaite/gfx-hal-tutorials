8f4b4ba740a6b8446e3a38e6f14e6253a8725e0c
cthwaite <thwaite@mac.com>
2018-10-04 21:06:40 +0100

    Create a window

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+#[cfg(windows)]
+extern crate gfx_backend_dx12 as backend;
+#[cfg(target_os = "macos")]
+extern crate gfx_backend_metal as backend;
+#[cfg(all(unix, not(target_os = "macos")))]
+extern crate gfx_backend_vulkan as backend;
+
+extern crate gfx_hal;
+extern crate winit;
+
+// There are a lot of imports - best to just accept it.
+use gfx_hal::{
+    command::{ClearColor, ClearValue},
+    format::{Aspects, ChannelType, Format, Swizzle},
+    image::{Access, Layout, SubresourceRange, ViewKind},
+    pass::{
+        Attachment, AttachmentLoadOp, AttachmentOps, AttachmentStoreOp, Subpass, SubpassDependency,
+        SubpassDesc, SubpassRef,
+    },
+    pool::CommandPoolCreateFlags,
+    pso::{
+        BlendState, ColorBlendDesc, ColorMask, EntryPoint, GraphicsPipelineDesc, GraphicsShaderSet,
+        PipelineStage, Rasterizer, Rect, Viewport,
+    },
+    queue::Submission,
+    Backbuffer, Device, FrameSync, Graphics, Instance, Primitive, Surface, SwapImageIndex,
+    Swapchain, SwapchainConfig,
+};
+
+use winit::{Event, EventsLoop, KeyboardInput, VirtualKeyCode, WindowBuilder, WindowEvent};
+
+
+fn main() {
+    let mut events_loop = EventsLoop::new();
+    let window = WindowBuilder::new()
+                    .with_title("Part 00: Triangle")
+                    .with_dimensions((256, 256).into())
+                    .build(&events_loop)
+                    .unwrap();
+
+    loop {
+        let mut quitting = false;
+        events_loop.poll_events(|event| {
+            if let Event::WindowEvent { event, .. } = event {
+                match event {
+                    WindowEvent::CloseRequested => quitting = true,
+                    WindowEvent::KeyboardInput {
+                        input: KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                        ..
+                    } => quitting = true,
+                    _ => ()
+                }
+
+            }
+        });
+
+        if quitting {
+            break;
+        }
+    }
+}
