b2bfe42030bd4a36e7022b68668972613cbf752c
cthwaite <thwaite@mac.com>
2018-10-07 19:20:33 +0100

    Vertex buffers example uses buffer utils

--- src/bin/02_vertex_buffers.rs
+++ src/bin/02_vertex_buffers.rs
@@ -1,11 +1,15 @@
 extern crate haltut;
 
 use haltut::prelude::*;
+
 use haltut::backend;
+use backend::Backend;
+use haltut::utils;
+
 
 static WIN_TITLE : &'static str = "Part 02: Vertex Buffers";
-static VERT_SPIRV : &'static [u8] = include_bytes!("../../assets/gen/shaders/part02.vert.spv");
-static FRAG_SPIRV : &'static [u8] = include_bytes!("../../assets/gen/shaders/part02.frag.spv");
+static VERT_SPIRV : &'static [u8] = include_bytes!("../assets/gen/shaders/part02.vert.spv");
+static FRAG_SPIRV : &'static [u8] = include_bytes!("../assets/gen/shaders/part02.frag.spv");
 
 // repr(C) ensures deterministic layout in memory.
 #[derive(Clone, Copy, Debug)]
@@ -196,83 +200,18 @@ fn main() {
               .unwrap()
     };
 
-
-    // Next, we create a vertex buffer to upload our mesh data into. We need
-    // both the buffer and the memory it's using in order to destroy them when
-    // we're done.
-    //
-    // Your graphics card provides different types of memory, with some being more
-    // efficient for certain tasks. For example, CPU_VISIBLE memory will allow you
-    // to write to it directly but be slow to use for rendering, while DEVICE_LOCAL
-    // memory is fast for rendering, but you'll require a staging buffer to copy data
-    // into it.
-    //
-    // We get a list of the available memory types here so we can choose one later.
     let memory_types = physical_device.memory_properties().memory_types;
 
     let mesh = MESH;
 
     // Here's where we create the buffer itself, and the memory it uses.
-    let (vertex_buffer, vertex_buffer_memory) = {
-        // First, create an unbound buffer (i.e. a buffer not currently bound to
-        // any memory). We need to indicate the size in bytes, and declare that
-        // we want to use it for vertex data.
-        let item_count = mesh.len();
-        let stride = std::mem::size_of::<Vertex>() as u64;
-        let buffer_len = item_count as u64 * stride;
-        let unbound_buffer = device.create_buffer(buffer_len, buffer::Usage::VERTEX)
-                                   .unwrap();
-
-        // Next, we need the graphics card to tell us what the memory requirements
-        // for this buffer are. This includes the size, alignment, and available
-        // memory types. We know how big our data is, but we have to store it in
-        // a valid way for the device.
-        let req = device.get_buffer_requirements(&unbound_buffer);
-
-        // This complicated looking statement filters through memory types to pick
-        // one that's appropriate. We call enumerate to give us the ID (the index)
-        // of each type, which might look something like this:
-        //
-        // id   ty
-        // ==   ==
-        // 0    DEVICE_LOCAL
-        // 1    COHERENT | CPU_VISIBLE
-        // 2    DEVICE_LOCAL | CPU_VISIBLE
-        // 3    DEVICE_LOCAL | CPU_VISIBLE | CPU_CACHED
-        //
-        // We then want to find the first type that is supported by out memory
-        // requirements (e.g, `id` is in the `type_mask` bitfield), and also has
-        // the CPU_VISIBLE property (so we can copy vertex data directly into it.)
-        let upload_type = memory_types.iter()
-                                      .enumerate()
-                                      .find(|(id, ty)| {
-                                          let type_supported = req.type_mask & (1_u64 << id) != 0;
-                                          type_supported && ty.properties.contains(Properties::CPU_VISIBLE)
-                                      })
-                                      .map(|(id, _ty)| MemoryTypeId(id))
-                                      .expect("Could not find appropriate vertex buffer memory type.");
-
-        // Now that we know the type and size of memory that we need, we can
-        // allocate it, and bind the buffer to it.
-        // The `0` here is an offset, which could be used to bind multiple
-        // buffers to the same blck of memory.
-        let buffer_memory = device.allocate_memory(upload_type, req.size).unwrap();
-        let buffer = device.bind_buffer_memory(&buffer_memory, 0, unbound_buffer)
-                           .unwrap();
-
-        // Finally, we can copy our vertex data into the buffer. To do this, we
-        // get a writer corresponding to the range of memory we want to write
-        // to. This writer essentially memory-maps the data and acts as a slice
-        // that we can write into.
-        // Once we do that, we unmap the memory, and our buffer should be full.
-        {
-            let mut dest = device.acquire_mapping_writer::<Vertex>(&buffer_memory, 0..buffer_len)
-                                 .unwrap();
-            dest.copy_from_slice(mesh);
-            device.release_mapping_writer(dest);
-        }
-        (buffer, buffer_memory)
-    };
+    let (vertex_buffer, vertex_buffer_memory) = utils::create_buffer::<Backend, Vertex>(
+        &device,
+        &memory_types,
+        Properties::CPU_VISIBLE,
+        buffer::Usage::VERTEX,
+        mesh
+    );
 
     let frame_semaphore = device.create_semaphore();
     let frame_fence = device.create_fence(false);
