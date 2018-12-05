f1b80150fc43a7261b1b3b8efd35c13d8aebe536
cthwaite <thwaite@gmail.com>
2018-12-05 22:48:28 +0000

    Remove main.rs, make all backends public

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #[cfg(windows)]
-extern crate gfx_backend_dx12 as gfx_backend;
+pub extern crate gfx_backend_dx12 as gfx_backend;
 #[cfg(target_os = "macos")]
 pub extern crate gfx_backend_metal as gfx_backend;
 #[cfg(all(unix, not(target_os = "macos")))]
-extern crate gfx_backend_vulkan as gfx_backend;
+pub extern crate gfx_backend_vulkan as gfx_backend;
 
 extern crate gfx_hal;
 extern crate winit;
